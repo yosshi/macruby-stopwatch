cdea7ee703bb2fa6b743d98c188847cf8fbb6fab
Yoshiyuki MIYAGI <yosshi1202@gmail.com>
2013-01-09 01:50:22 +0900

    Initial Commit

---
+++ StopWatch/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  StopWatch
+#
+#  Created by Yoshiyuki MIYAGI on 2013/01/09.
+#  Copyright 2013年 Yoshiyuki MIYAGI. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
